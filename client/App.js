4fd13128d1c599426effb68d49fab81b8c9d5147
walidgzih12 <walidgzih3@gmail.com>
2024-04-30 11:59:42 +0100

    done

--- client/App.js
+++ client/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
  import { createNativeStackNavigator } from '@react-navigation/native-stack';
  import HomeScreen from './src/screens/HomeScreen';
- import DetailsScreen from './src/screens/DetailsScreen';
+//  import DetailsScreen from './src/screens/DetailsScreen';
  import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { View,Text } from 'react-native';
-const Tab = createBottomTabNavigator();
 
+const Tab = createBottomTabNavigator();
 const App = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        {/* <Stack.Screen name="OnBoardScreen" component={OnBoardScreen} /> */}
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={{headerShown: false}}>
+        {/* <Tab.Screen name="OnBoardScreen" component={OnBoardScreen} /> */}
+        <Tab.Screen name="HomeScreen" component={HomeScreen} />
+        {/* <Tab.Screen name="DetailsScreen" component={DetailsScreen} /> */}
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
