2092dc6a56db03566e3ccc11222279dc4fe1aac0
youssefnaili <nailiyoussef01@gmail.com>
2024-05-06 10:14:15 +0100

    d

--- client/App.js
+++ client/App.js
@@ -31,7 +31,13 @@ import first from './src/components/first/frPage'
 import two from './src/components/two/two'
 import login from "./src/components/Authentification/login";
 import signup from "./src/components/Authentification/signup";
-
+import ProfileDetails from './src/components/profile/profileDetails';
+import Listings from './src/components/profile/Listings';
+import Contact from './src/components/profile/Contact';
+import Search from './src/components/profile/SearchBar';
+import NotificationPage from './src/components/profile/Notification';
+import Apartment from './src/components/profile/Apartment';
+import Lands from './src/components/profile/Land';
 
 function App() {
 
@@ -47,7 +53,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
  
-   <Stack.Screen
+   {/* <Stack.Screen
           name="login"
           component={login}
           options={{ headerShown: false }} 
@@ -56,9 +62,17 @@ function App() {
           name="signup"
           component={signup}
           options={{ title: 'Sign Up' }} 
-        />
-    
-       
+        /> */}
+    <Stack.Screen
+          name="ProfileDetails"
+          component={ProfileDetails}
+          options={{ title: '' }} // Customize the title if needed
+        /> 
+         <Stack.Screen name="Listings" component={Listings} />
+        <Stack.Screen name="Contact" component={Contact} />
+        <Stack.Screen name="Apartment" component={Apartment} />
+        <Stack.Screen name="Notifications" component={NotificationPage} />
+        <Stack.Screen name="Land" component={Lands} />
       </Stack.Navigator>
 
     
