e9c50aa0fbeafccd2213b8e1629d9a84a87c37bd
MohamedBensassi <medbensassi99@gmail.com>
2024-05-07 10:00:36 +0100

    d

--- client/App.js
+++ client/App.js
@@ -25,115 +25,33 @@ import DetailsScreen from "./src/screens/DetailsScreen";
 import RecommendedScreen from "./src/screens/RecommendedScreen";
 import Chat from "./src/components/chat/chat";
 import Chatroom from "./src/components/chat/allrooms";
+import HomeTabs from './hpmetaps';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-// Tab Navigator for logged-in users
-function HomeTabs() {
-  return (
-
-
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-          let Component = Icon; 
-          let Component = Icon;  
-          switch (route.name) {
-            case 'HomeScreen':
-              iconName = focused ? 'home' : 'home-outline';
-              break;
-            case 'Details':
-              iconName = 'information-outline';
-              break;
-            case 'Recommended':
-              iconName = 'star-outline';
-              break;
-              case 'Chatroom':  
-              iconName = 'chatbox-ellipses-outline';
-              Component = Icon2;
-            case 'Chatroom':  
-            iconName = 'chatbox-ellipses-outline';
-            Component = Icon2;
-              break;
-              
-              default:
-                iconName = 'alert-circle-outline';
-                break;
-              }
-              return <Component name={iconName} size={size} color={color} />;
-            },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
-      <Tab.Screen name="Details" component={DetailsScreen} />
-      <Tab.Screen name="Recommended" component={RecommendedScreen} />
-      <Tab.Screen name="Chatroom" component={Chatroom} />
-      {/* <Tab.Screen name="Chat" component={Chat} /> */}
-    </Tab.Navigator>
- 
-  );
-}
 
 // Main stack navigator
 function App() {
   return (
-    <NavigationContainer>
-<Stack.Screen
-      name="ProfileDetails"
-      component={ProfileDetails}
-      options={{ title: '' }} 
-    /> 
-     <Stack.Screen name="Listings" component={Listings} />
-    <Stack.Screen name="Contact" component={Contact} />
-    <Stack.Screen name="Apartment" component={Apartment} />
-    <Stack.Screen name="Notifications" component={NotificationPage} />
-    <Stack.Screen name="Land" component={Lands} />
-      <Stack.Navigator initialRouteName="FrPage">
-        <Stack.Screen name="FrPage" component={FrPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Two" component={Two} options={{ headerShown: false }} />
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
-        <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }} />
-        <Stack.Screen name='chat' component={Chat} options={{ headerShown: false }}     />
-        <Stack.Screen name='Details' component={DetailsScreen} options={{headerShown : false}} />
-
-        <Stack.Screen 
-          name='RecommendedScreen'
-          component={RecommendedScreen}
-          options={({ navigation }) => ({
-            headerShown: true,
-            title: 'Recommended Houses',
-            headerStyle: {
-              backgroundColor: '#faebd7', 
-              shadowColor: '#000', 
-              shadowOffset: { width: 0, height: 2 }, 
-              shadowOpacity: 0.1, 
-              shadowRadius: 3, 
-              elevation: 5, 
-            },
-            headerTitleStyle: {
-              fontWeight: 'bold', 
-              fontSize: 22, 
-              fontFamily: 'Roboto, "Helvetica Neue", sans-serif', 
-            },
-            headerTintColor: 'black', 
-            headerLeft: () => (
-              <Icon
-                name="arrow-back-sharp"
-                size={24}
-                color="black"
-                onPress={() => navigation.goBack()}
-                style={{ marginLeft: 15 }}
-              />
-            ),
-          })}
-      />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="FrPage">
+          <Stack.Screen name="FrPage" component={FrPage} options={{ headerShown: false }} />
+          <Stack.Screen name="Two" component={Two} options={{ headerShown: false }} />
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+          <Stack.Screen name="Signup" component={Signup} options={{ headerShown: false }} />
+          <Stack.Screen name="HomeTabs" component={HomeTabs} options={{ headerShown: false }} />
+          <Stack.Screen name='chat' component={Chat} options={{ headerShown: false }} />
+          <Stack.Screen name='Details' component={DetailsScreen} options={{ headerShown: false }} />
+          <Stack.Screen name='ProfilDetail' component={ProfileDetails} options={{ headerShown: false }}  />
+          <Stack.Screen 
+            name='RecommendedScreen'
+            component={RecommendedScreen}
+          options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  )
 }
 
 export default App;
